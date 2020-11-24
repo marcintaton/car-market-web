977d03230594d9ef63e0cc8c99ac593416a06b2d
Marcin Tatoń <tatonmarcinpr@gmail.com>
2020-11-24 19:57:05 +0100

    App initialized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { store } from "./actions/store";
+import { Provider } from "react-redux";
+import CarListings from './components/CarListings';
+import { Container, Switch } from '@material-ui/core';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Router } from '@material-ui/icons';
+import LoginPage from './components/LoginPage';
+import ListingDetails from './components/ListingDetails';
+import Navbar from './components/Navbar';
+import UserDetails from './components/UserDetails';
+import CarListingForm from './components/CarListingForm';
+import CarListingEdit from './components/CarListingEdit';
+import { ToastProvider } from 'react-toast-notifications';
+import Chat from './components/Chat';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ToastProvider autoDismiss={true}>
+        <Container maxWidth="lg">
+          <BrowserRouter>
+            <Navbar />
+            <Route exact path='/' component={CarListings} />
+            <Route path='/login' component={LoginPage} />
+            <Route path='/listing/:id' component={ListingDetails} />
+            <Route path='/user/:id' component={UserDetails} />
+            <Route path='/addListing' component={CarListingForm} />
+            <Route path='/listing/:id/edit' component={CarListingEdit} />
+            <Route path='/chat' component={Chat} />
+          </BrowserRouter>
+        </Container>
+      </ToastProvider>
+    </Provider>
   );
 }
 
