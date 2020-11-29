d261b6952514c9798dbc9dbcda7114a8f575717c
Marcin Tatoń <tatonmarcinpr@gmail.com>
2020-11-29 22:53:30 +0100

    documented carlisting actions

--- src/actions/CarListing.js
+++ src/actions/CarListing.js
@@ -1,6 +1,10 @@
 import { LISTING_ACTION_TYPES } from "../ActionTypes"
 import api from "./api"
 
+/**
+ * action that makes an API call 
+ * to get all car listings form the database
+ */
 export const fetchAll = () => dispatch => {
     api.carListing().fetchAll()
         .then(
@@ -14,6 +18,11 @@ export const fetchAll = () => dispatch => {
         .catch(error => console.log(error))
 }
 
+/**
+ * action that makes an API call 
+ * to get spcific listing by Id
+ * @param {*} x Id to fetch 
+ */
 export const fetch = (x) => dispatch => {
     api.carListing().fetchById(x)
         .then(
@@ -27,6 +36,11 @@ export const fetch = (x) => dispatch => {
         .catch(error => console.log(error))
 }
 
+/**
+ * Formats data received form form
+ * to prepare it for insertion into database
+ * @param {*} data data from form
+ */
 const formatData = data => ({
     ...data,
     dateAdded: parseInt(data.dateAdded ? data.dateAdded : 0),
@@ -36,6 +50,12 @@ const formatData = data => ({
     price: parseInt(data.price ? data.price : 0),
 })
 
+/**
+ * action that makes an API call 
+ * to create new car listing row from passed data
+ * @param {*} data 
+ * @param {*} onSuccess 
+ */
 export const create = (data, onSuccess) => dispatch => {
     data = formatData(data)
     console.log(data.userId)
@@ -49,6 +69,13 @@ export const create = (data, onSuccess) => dispatch => {
     }).catch(error => console.log(error))
 }
 
+/**
+ * action that makes an API call 
+ * to update existing row in database 
+ * @param {*} id Id of listing to modify
+ * @param {*} data updated data
+ * @param {*} onSuccess callback to execute on successful update
+ */
 export const update = (id, data, onSuccess) => dispatch => {
     data = formatData(data)
     api.carListing().update(id, data).then(response => {
@@ -60,6 +87,12 @@ export const update = (id, data, onSuccess) => dispatch => {
     }).catch(error => console.log(error))
 }
 
+/**
+ * action that makes an API call 
+ * to delete existing row in database 
+ * @param {*} id Id of listing to delete
+ * @param {*} onSuccess callback to execute on successful deletion
+ */
 export const deleteListing = (id, onSuccess) => dispatch => {
     api.carListing().delete(id).then(response => {
         dispatch({
@@ -70,6 +103,10 @@ export const deleteListing = (id, onSuccess) => dispatch => {
     }).catch(error => console.log(error))
 }
 
+/**
+ * store current filter settings
+ * @param {*} filterString settings to sotre
+ */
 export const filter = (filterString) => dispatch => {
     dispatch({
         type: LISTING_ACTION_TYPES.FILTER,
